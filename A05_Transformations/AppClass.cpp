29e6b0d9ea356b90ae9c07fc62c412067dc00a03
Maximilian Wright <twixthehero@gmail.com>
2016-02-28 16:57:49 -0500

    Finished A05

--- A05_Transformations/AppClass.cpp
+++ A05_Transformations/AppClass.cpp
@@ -51,9 +51,11 @@ void AppClass::Update(void)
 #pragma region YOUR CODE GOES HERE
 	//Calculate the position of the Earth
 	m_m4Earth = glm::rotate(IDENTITY_M4, m_fEarthTimer, vector3(0.0f, 1.0f, 0.0f));
+    m_m4Earth = m_m4Earth * distanceEarth;
 
 	//Calculate the position of the Moon
 	m_m4Moon = glm::rotate(IDENTITY_M4, m_fMoonTimer, vector3(0.0f, 1.0f, 0.0f));
+    m_m4Moon = m_m4Earth * m_m4Moon * distanceMoon;
 #pragma endregion
 
 #pragma region Print info
